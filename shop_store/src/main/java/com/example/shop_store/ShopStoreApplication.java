9da0fc129adecd9e81f90d74e6cc12080ed04217
Kudaybergen <131019311+Kudaybergen@users.noreply.github.com>
2023-05-01 19:59:50 +0600

    Add files via upload

---
+++ shop_store/src/main/java/com/example/shop_store/ShopStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopStoreApplication.class, args);
+	}
+
+}
