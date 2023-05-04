059eccac30ac070f52b4bb8f72fb935ceb439864
test2 <test@mail.ru>
2023-05-04 22:38:22 +0600

    updated pages catalog and add_good, backend works fine

--- shop_store/src/main/java/com/example/shop_store/controllers/AuthController.java
+++ shop_store/src/main/java/com/example/shop_store/controllers/AuthController.java
@@ -13,14 +13,4 @@ public class AuthController {
     public String index(){
         return "index";
     }
-
-    @GetMapping("/test")
-    public String test(){
-        return "test";
-    }
-
-    @GetMapping("/catalog")
-    public String catalog(){
-        return "catalog";
-    }
 }
--- shop_store/src/main/java/com/example/shop_store/controllers/MainController.java
+++ shop_store/src/main/java/com/example/shop_store/controllers/MainController.java
@@ -1,8 +1,10 @@
 package com.example.shop_store.controllers;
 
+import com.example.shop_store.domain.Goods;
 import com.example.shop_store.repos.GoodsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +19,20 @@ public class MainController {
         return "addGood";
     }
     @PostMapping("/add_good")
-    public String addGoodP(@RequestParam(value = "goodName", required = false) String goodName){
-        System.out.println("working" + goodName);
-        return "index";
+    public String addGoodP(@RequestParam(value = "goodName", required = false) String goodName,
+                           @RequestParam(value = "goodDesc", required = false) String goodDesc,
+                           @RequestParam(value = "goodPrice", required = false) Integer price){
+        System.out.println(goodDesc + goodName + price);
+        Goods goods = new Goods(goodName, goodDesc, price);
+        goodsRepo.save(goods);
+        return "redirect:/";
+    }
+
+    @GetMapping("/catalog")
+    public String catalog(Model model){
+        Iterable<Goods> goods = goodsRepo.findAll();
+        System.out.println(goods);
+        model.addAttribute("goods", goods);
+        return "catalog";
     }
 }
