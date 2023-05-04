a25a8b6279e98a5ebe8888e075b7cb460bcd451c
test2 <test@mail.ru>
2023-05-04 22:54:09 +0600

    added link for more information in /catalog page. Endpoint also works correctly

--- shop_store/src/main/java/com/example/shop_store/controllers/MainController.java
+++ shop_store/src/main/java/com/example/shop_store/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -34,5 +35,11 @@ public class MainController {
         System.out.println(goods);
         model.addAttribute("goods", goods);
         return "catalog";
+    }
+
+    @GetMapping("/catalog/{id}")
+    public String catalogView(@PathVariable Integer id){
+        System.out.println(id);
+        return "catalogView";
     }
 }
